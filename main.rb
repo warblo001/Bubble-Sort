e96658cdd2c3d4f6e482ffe1b7b4a3f6874694c6
warblo001 <67009988+warblo001@users.noreply.github.com>
2020-11-23 08:45:40 +0100

    Update Files

--- main.rb
+++ main.rb
@@ -4,17 +4,18 @@ def bubble_sort(arr)
       arr[y - 1], arr[y] = arr[y], arr[y - 1] if arr[y - 1] > arr[y]
     end
   end
-  print arr
+  arr
 end
 
 def bubble_sort_by(arr)
   (1..(arr.length - 1)).each do |y|
     arr[y - 1], arr[y] = arr[y], arr[y - 1] if (yield arr[y - 1], arr[y]).positive?
   end
-  print arr
+  arr
 end
 
-puts bubble_sort([4, 3, 78, 2, 0, 2])
-bubble_sort_by %w[hi hello hey] do |left, right|
+p bubble_sort([4, 3, 78, 2, 0, 2])
+x = bubble_sort_by %w[hi hello hey] do |left, right|
   left.length - right.length
 end
+p x
